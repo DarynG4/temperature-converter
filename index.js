287c1981882815d95d74745513b92cb518b48f23
Daryn Gregory <daryn.greg@icloud.com>
2025-12-11 00:37:37 -0500

    Added step 3 JSDoc

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ function convertToCelsius(fahrenheit) {
   //   let celsius = (fahrenheit - 32) * (5 / 9);
   return (fahrenheit - 32) * (5 / 9);
 }
+/**
+ * if temperature in celsius is < 0 then it is very cold
+ * if temperature in celsius is < 20 then it is cold
+ * if temperature in celsius is < 30 then it is warm
+ * if temperature in celsius is < 40 then it is hot
+ * if temperature in celsius is >= 40 then it is very hot
+ * @param {number} temperature in fahrenheit
+ * @returns {string} a description of the temperature according to celsius
+ */
