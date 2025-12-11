ed9f87cb271f7f6eb7cd71d0946e139001fd3765
Daryn Gregory <daryn.greg@icloud.com>
2025-12-11 00:35:23 -0500

    Completed step 2

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * celsius = (fahrenheit - 32) * (5 / 9)
+ * @param {number} temperature in fahrenheit
+ * @returns {number} the given temperature now converted to celsius
+ */
+function convertToCelsius(fahrenheit) {
+  //   let celsius = (fahrenheit - 32) * (5 / 9);
+  return (fahrenheit - 32) * (5 / 9);
+}
